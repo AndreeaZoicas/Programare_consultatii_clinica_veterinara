efa8899f153be5ebce4f9949f3152601cbab8803
AndreeaZoicas <zoicas.andree@yahoo.com>
2025-01-13 16:42:23 +0200

    V1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Programare_consultatii_clinica_veterinara.Data;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Configure Identity with LibraryIdentityContext
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+    options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<LibraryIdentityContext>();
+
+// Configure LibraryIdentityContext with the correct connection string
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryIdentityContextConnection")
+    ?? throw new InvalidOperationException("Connection string 'LibraryIdentityContextConnection' not found.")));
+
+// Configure Programare_consultatii_clinica_veterinaraContext with its connection string
 builder.Services.AddDbContext<Programare_consultatii_clinica_veterinaraContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Programare_consultatii_clinica_veterinaraContext") ?? throw new InvalidOperationException("Connection string 'Programare_consultatii_clinica_veterinaraContext' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Programare_consultatii_clinica_veterinaraContext")
+    ?? throw new InvalidOperationException("Connection string 'Programare_consultatii_clinica_veterinaraContext' not found.")));
 
 var app = builder.Build();
 
